2571687d35bbc00fa81f107a4f5c029889e2d6f3
Abdulloh Ortiqov <179808048+Abdulloh-dev03@users.noreply.github.com>
2026-04-02 18:01:17 +0500

    Add async rewrites for API requests

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,15 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   turbopack:{
     root:"."
-  }
+  },
+ async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://ai-teach-atelier.onrender.com/:path*',
+      },
+    ]
+  },
 }
  
 export default nextConfig
