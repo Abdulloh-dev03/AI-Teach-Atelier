ce705c133e1258615be8339e64ec4d64c4bb1a41
Abdulloh-dev03 <abdullohortqiov15@gmail.com>
2026-04-13 20:59:19 +0500

    fixed next.config file

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,22 +1,22 @@
 import type { NextConfig } from "next";
 
-const backendOrigin = (
-  process.env.BACKEND_URL ??
-  process.env.NEXT_PUBLIC_API_URL ??
-  "http://localhost:4000"
-)
-  .replace(/\/api\/?$/, "")
-  .replace(/\/+$/, "");
-
 const nextConfig: NextConfig = {
   turbopack: {
-    root: __dirname,
+    root:".",
   },
   async rewrites() {
+    const backendOrigin = (
+      process.env.BACKEND_URL ??
+      process.env.NEXT_PUBLIC_API_URL ??
+      "http://localhost:4000"
+    )
+      .replace(/\/api\/?$/, "")
+      .replace(/\/+$/, "");
+
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:4000/api/:path*",
+        destination: `${backendOrigin}/api/:path*`,
       },
     ];
   },
